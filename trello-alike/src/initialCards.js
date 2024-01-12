215cbffb36aa7bb6c2ee1015ff9934e6e24805b2
souchardani <114416167+souchardani@users.noreply.github.com>
2024-01-12 14:03:22 +0100

    cambios

--- trello-alike/src/initialCards.js
+++ trello-alike/src/initialCards.js
@@ -3,14 +3,13 @@ export default {
     { id: 1, title: "Card 1", description: "Card 1 description" },
     { id: 2, title: "Card 2", description: "Card 2 description" },
     { id: 3, title: "Card 3", description: "Card 3 description" },
+  ],
+  dev: [
     { id: 4, title: "Card 4", description: "Card 4 description" },
     { id: 5, title: "Card 5", description: "Card 5 description" },
   ],
-  dev: [
-    { id: 1, title: "Card 1", description: "Card 1 description" },
-    { id: 2, title: "Card 2", description: "Card 2 description" },
-    { id: 3, title: "Card 3", description: "Card 3 description" },
-    { id: 4, title: "Card 4", description: "Card 4 description" },
-    { id: 5, title: "Card 5", description: "Card 5 description" },
+  testes: [
+    { id: 6, title: "Card 6", description: "Card 6 description" },
+    { id: 7, title: "Card 7", description: "Card 7 description" },
   ],
 };
